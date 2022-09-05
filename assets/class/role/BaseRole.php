189dbf693d5acbbe5cd52ba275b4e3fe89a07243
jay_chang <jay_chang@allmarketing.com.tw>
2022-09-05 15:44:57 +0800

    角色基礎抽象類別

---
+++ assets/class/role/BaseRole.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\assets\role;
+
+abstract class BaseRole
+{
+    private $name;
+    private $sex;
+    private $visible;
+    private $img;
+    private $skill;
+    private $mission;
+}
